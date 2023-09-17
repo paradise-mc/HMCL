fd89ed5febc23ad260d0f89c3344811cc346ece8
Zero <kanade@acgme.cn>
2023-09-17 14:11:08 +0800

    feat: update

--- HMCL/build.gradle.kts
+++ HMCL/build.gradle.kts
@@ -26,7 +26,7 @@ val buildNumber = System.getenv("BUILD_NUMBER")?.toInt().let { number ->
         if (!shortCommit.isNullOrEmpty()) "$prefix-$shortCommit" else "SNAPSHOT"
     }
 }
-val versionRoot = System.getenv("VERSION_ROOT") ?: "1.0"
+val versionRoot = System.getenv("VERSION_ROOT") ?: "2023.09.17a"
 //val versionType = System.getenv("VERSION_TYPE") ?: if (isOfficial) "nightly" else "unofficial"
 val versionType = "stable"
 
