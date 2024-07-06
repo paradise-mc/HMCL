1b9d9844c8063445a37e737b3bb28e6f817bc472
TiltySola <tilty.sola@gmail.com>
2024-07-06 22:22:28 +0900

    feat: update

--- HMCL/build.gradle.kts
+++ HMCL/build.gradle.kts
@@ -27,7 +27,7 @@ val buildNumber = System.getenv("BUILD_NUMBER")?.toInt().let { number ->
         if (!shortCommit.isNullOrEmpty()) "$prefix-$shortCommit" else "SNAPSHOT"
     }
 }
-val versionRoot = System.getenv("VERSION_ROOT") ?: "2024.07.02a"
+val versionRoot = System.getenv("VERSION_ROOT") ?: "2024.07.06a"
 //val versionType = System.getenv("VERSION_TYPE") ?: if (isOfficial) "nightly" else "unofficial"
 val versionType = "stable"
 
